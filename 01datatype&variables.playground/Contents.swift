89a29cc186016b93c7143ce1cbccdf9a9e31c153
Gia Schreitmueller <giaschreitmueller@Apples-MacBook-Pro-94.local>
2019-08-06 10:08:51 -0400

    Day 1 Lessons

---
+++ 01datatype&variables.playground/Contents.swift
@@ -0,0 +1,84 @@
+import UIKit
+
+var str = "Hello, playground"
+//Examples of strings
+"puppies"
+"1234"
+"Hello, world"
+
+//Examples of a variable
+var address = "10 Hudson Yard"
+address = "48th floor"
+var companyName : String
+companyName = "Best Company Ever"
+
+//Example of a Constant
+let planet = "Earth"
+
+//Examples of Joining variables and strings
+var string1 = "Welcome"
+var string2 = "Gia Schreitmueller"
+
+var welcome1 = "\(string1) \(string2)"
+var welcome2 = string1 + " " + string2
+
+print(welcome1)
+print("here is my sentence")
+
+//Practice
+var first = "Karlie"
+var last = "Kloss"
+
+print("\(first)\(last)")
+print(last + "" + first)
+print(first + " " + last)
+print( last + " " + first + " " + last + " " + first)
+
+var me = "I"
+var heart = "love"
+print(me + " " + heart + " " + first)
+
+// Examples of Integers
+1
+54
+-16
+
+//Examples of Floats and Doubles
+0.13
+3.14
+2.1
+
+var integer = 4.0
+var double = 5.0
+
+4 * 4
+4 * integer
+4 * double
+integer * double
+
+//Practice with numbers
+var a = 12
+var b = 65
+var c = 31
+var d = 98
+var e = 4
+
+(a + b + c + d) / 4
+
+a + b * (c / d)
+
+//Practice Data Types
+//let name = "Gia"
+//let Birthday = "August 19 2005"
+//var eyeColor = "purple and hazel"
+//var address = "10 Hudson Yards"
+//var gradeLevel = "9th grade"
+//var Age = "13"
+//var favoriteColor = "purple"
+
+//Getting Paid
+10.25 * 20
+
+var name = "Beyonce"
+//print("happy birthday to you, happy birthday to you, happy birth
+
---
+++ MyPlayground.conditionals.playground/Contents.swift
@@ -0,0 +1,66 @@
+//import UIKit
+
+//var str = "Hello, playground"
+//
+//5 < 3
+//12 > 7
+//6 != 8
+////7 == "7"
+//"karlie" == "karlie"
+//"Karlie" == "karlie"
+//var luckyNum = 7
+//luckyNum < 10
+//luckyNum == 7
+
+//Examples of Conditionals
+//
+//var dogAge = 5
+//if dogAge < 2 {
+//    print("You are a puppy!🐶")
+//}
+//else if dogAge > 12 {
+//    print("You are elderly.")
+//}
+////else {
+////    print("You are awesome!")
+//}
+
+// Practice
+
+//var favoriteFood = "Starbucks"
+//if favoriteFood == "Chipotle" {
+//    print("Chipotle is pretty good")
+//}
+//else if favoriteFood == "Starbucks" {
+//    print("Starbucks is OK.")
+//}
+//else if favortieFood == "Starbucks" {
+//    print("Food is great!!")
+//}
+//
+var hasAccount = "true"
+if hasAccount == "true" {
+    print("Please Log in!")
+}
+var grade = "0"
+if grade == "7" {
+    print("You are in 7th grade.")
+}
+else if grade == "6" {
+    print("You are in 6th grade")
+}
+else if grade == "8" {
+    print("you are in 8th grade.")
+}
+else if grade >= "9" {
+    print("You are in highschool.")
+}
+else if grade == "0" {
+    print("You are in Kindergarten")
+}
+else {
+    print("You are in Elementary School!")
+}
+
+
+
